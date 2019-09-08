5b9551effdeebabb9a055cf99682e47f477a3bba
Marni Swamy <ayyappa.marni@hotmail.com>
2019-09-08 10:35:09 +0200

    jest configured

--- src/selectors/statusFilterSelector/__tests__/statusFilterSelector.test.js
+++ src/selectors/statusFilterSelector/__tests__/statusFilterSelector.test.js
@@ -3,22 +3,18 @@ import { statusFilterSelector } from '../statusFilterSelector';
 describe('statusFilterSelector', () => {
   it('should select data from the store state', () => {
     const mockState = {
-      mockParam: {
-        mockData: 'mockString',
-      },
+      selectedStatus: 'Active',
     };
     const expected = statusFilterSelector(mockState);
-    const result = 'mockString';
+    const result = 'Active';
     expect(expected).toEqual(result);
   });
   it('should provide a default value if there is no value in the store', () => {
     const mockState = {
-      mockParam: {
-        mockData: 'mockString',
-      },
+      selectedStatus: 'Inactive',
     };
     const expected = statusFilterSelector(mockState);
-    const result = '';
+    const result = 'Inactive';
     expect(expected).toEqual(result);
   });
 });
